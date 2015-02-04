211357f370034903cf1d195c6278b9df5d9a2a18
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-02-04 13:40:19 -0500

    almost there

--- scripts/mx/conflate_mx.sh
+++ scripts/mx/conflate_mx.sh
@@ -1,28 +1,32 @@
 #! bin/bash
 # sudo pip install csvkit
-# mkdir csv
-# mkdir inegi
-# cd inegi
+mkdir csv
+mkdir inegi
+cd inegi
 
-# mapbox auth production XXXXXX
-# aws s3 cp s3://gis-data/inegi/inegi_data.zip
+#mapbox auth production XXXXXX
+#aws s3 cp s3://gis-data/inegi/inegi_data.zip
 
 unzip inegi_data.zip;
 
 for f in inegi_data/*.zip; do
     unzip $f -d inegi_data/;
-done;
+done
 
 rm inegi_data/*.zip;
 
+cd inegi_data
 counter=0
-
-for f in inegi_data/*T.shp; do
+for f in */*T.shp; do
     counter=$((counter+1))
-    output='csv/'$f'.csv';
-    ogr2ogr -t_srs EPSG:4326 -f CSV $output $f/*T.shp -lco GEOMETRY=AS_XY;
-    iconv -f 'iso-8859-1' -t utf8 $output > $output
+    out_name=$(basename $f .shp)
+    out_file='../csv/'$out_name'.csv';
+    ogr2ogr -t_srs EPSG:4326 -f CSV $out_file $f -lco GEOMETRY=AS_XY;
+    #iconv -f iso-8859-1 -t utf8 $out_file > $out_file
+    #iconv -f binary -t utf8 $out_file > $out_file
     echo $counter' done'
 done;
+cd ..
 
-csvstack csv/* > mx.csv
+#csvstack csv/* > mx.csv
+tail -n +2 csv/* | cat > mx.csv
