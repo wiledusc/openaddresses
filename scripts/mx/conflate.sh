60ee8e85833d8b37d9b5ad1308a1c5c7c9792144
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-16 13:18:57 -0400

    parallel unpack and convert

---
+++ scripts/mx/conflate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# aws s3 cp s3://gis-data/inegi/inegi_data.zip
+unzip inegi_data.zip && cd inegi_data
+parallel "unzip {}" ::: $(ls | grep 'zip')
+rm *.* # remove the zip files and rando files
+parallel "ogr2ogr -t_srs EPSG:4326 -f CSV /vsistdout/ {}/{}T.shp -lco GEOMETRY=AS_XY >> ../out.csv" ::: $(ls * | grep 'T\.shp' | sed s/T.shp//g)
