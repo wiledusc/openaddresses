358e85d85979ebacacae2ea382c37b1e9c4f5fb4
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-16 13:55:39 -0400

    minor verbosity

--- scripts/mx/conflate.sh
+++ scripts/mx/conflate.sh
@@ -4,4 +4,5 @@
 unzip inegi_data.zip && cd inegi_data
 parallel "unzip {}" ::: $(ls | grep 'zip')
 rm *.* # remove the zip files and rando files
+echo "reprojecting and converting to csv.."
 parallel "ogr2ogr -t_srs EPSG:4326 -f CSV /vsistdout/ {}/{}T.shp -lco GEOMETRY=AS_XY >> ../out.csv" ::: $(ls * | grep 'T\.shp' | sed s/T.shp//g)
